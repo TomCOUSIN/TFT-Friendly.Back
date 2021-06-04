87b17be72c9b6d1b51bfad5bd97bd9b736edf662
tomcousin <tom.cousin@epitech.eu>
2021-06-04 15:53:25 +0900

    [~] Update Program class by cleaning code and adding documentation comments

--- TFT-Friendly.Back/Program.cs
+++ TFT-Friendly.Back/Program.cs
@@ -9,18 +9,38 @@ using Microsoft.Extensions.Logging;
 
 namespace TFT_Friendly.Back
 {
+    /// <summary>
+    /// Program class
+    /// </summary>
     public class Program
     {
+        #region MAIN
+
+        /// <summary>
+        /// Main function of the program
+        /// </summary>
+        /// <param name="args">The arguments to use</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        #endregion MAIN
+
+        #region CREATE_HOST_BUILDER
+
+        /// <summary>
+        /// Create a Web Host Builder
+        /// </summary>
+        /// <param name="args">The arguments to use</param>
+        /// <returns>The newly created Web Host</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        #endregion CREATE_HOST_BUILDER
     }
 }
