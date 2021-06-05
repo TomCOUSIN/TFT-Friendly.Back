f8e7edcad923caf5baf653a18d361fdad44d40fe
tomcousin <tom.cousin@epitech.eu>
2021-06-05 12:03:34 +0900

    [~] Update UsersController by adding PATCH and DELETE routes

--- TFT-Friendly.Back/Controllers/UsersController.cs
+++ TFT-Friendly.Back/Controllers/UsersController.cs
@@ -55,7 +55,7 @@ namespace TFT_Friendly.Back.Controllers
         }
 
         /// <summary>
-        /// Post a new <see cref="User"/>
+        /// Post a new user
         /// </summary>
         /// <param name="user">The user to add</param>
         /// <returns>The newly created user</returns>
@@ -66,6 +66,34 @@ namespace TFT_Friendly.Back.Controllers
         {
             await _usersMongoService.InsertOneAsync(user);
             return CreatedAtAction(nameof(GetUsers), new {id = user.Id}, user);
+        }
+
+        /// <summary>
+        /// Update a specific user
+        /// </summary>
+        /// <param name="user">The updated user</param>
+        /// <returns>The updated user</returns>
+        /// <response code="200">Everything worked well</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpPatch]
+        public IActionResult UpdateUser(User user)
+        {
+            _usersMongoService.ReplaceOneAsync(user);
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// Delete a specific user
+        /// </summary>
+        /// <param name="user">The user to delete</param>
+        /// <returns>The deleted user</returns>
+        /// <response code="200">Everything worked well</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpDelete]
+        public IActionResult RemoveUser(User user)
+        {
+            _usersMongoService.DeleteOneAsync(user);
+            return Ok(user);
         }
 
         #endregion ROUTES
