b55ddbe3289e6f954fd53bd9e9b935b31fa0d32f
tomcousin <tom.cousin@epitech.eu>
2021-06-05 11:38:09 +0900

    [~] Update UsersController by adding database handling

--- TFT-Friendly.Back/Controllers/UsersController.cs
+++ TFT-Friendly.Back/Controllers/UsersController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TFT_Friendly.Back.Models.Users;
+using TFT_Friendly.Back.Services.Mongo;
 
 namespace TFT_Friendly.Back.Controllers
 {
@@ -11,17 +17,57 @@ namespace TFT_Friendly.Back.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        #region MEMBERS
+        
+        private readonly UsersMongoService _usersMongoService;
+        private readonly ILogger<UsersController> _logger;
+
+        #endregion MEMBERS
+
+        #region CONSTRUCTOR
+
         /// <summary>
-        /// Get a user
+        /// Initialize a new <see cref="UsersController"/> class
         /// </summary>
-        /// <returns>An empty string</returns>
+        /// <param name="logger">The logger to use</param>
+        /// <param name="usersMongoService">The users mongo database</param>
+        /// <exception cref="ArgumentNullException">Throw an exception if one of the parameter is null</exception>
+        public UsersController(ILogger<UsersController> logger, UsersMongoService usersMongoService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _usersMongoService = usersMongoService ?? throw new ArgumentNullException(nameof(usersMongoService));
+        }
+
+        #endregion CONSTRUCTOR
+        
+        #region ROUTES
+
+        /// <summary>
+        /// Get all the users
+        /// </summary>
+        /// <returns>The list of users</returns>
         /// <response code="200">Everything worked well</response>
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [Route("/sample")]
         [HttpGet]
-        public IActionResult GetEmptyUserName()
+        public async Task<IActionResult> GetUsers()
         {
-            return Ok("");
+            return Ok(await _usersMongoService.FindAsync());
         }
+
+        /// <summary>
+        /// Post a new <see cref="User"/>
+        /// </summary>
+        /// <param name="user">The user to add</param>
+        /// <returns>The newly created user</returns>
+        /// <response code="201">The user as been created successfully</response>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public async Task<IActionResult> PostUser(User user)
+        {
+            await _usersMongoService.InsertOneAsync(user);
+            return CreatedAtAction(nameof(GetUsers), new {id = user.Id}, user);
+        }
+
+        #endregion ROUTES
     }
 }
