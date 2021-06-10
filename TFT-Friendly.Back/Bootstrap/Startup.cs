bfd6b011cdd628dfe1cb22c12c988bd9918af259
tomcousin <tom.cousin@epitech.eu>
2021-06-10 19:54:11 +0900

    [+] Add Authorization and Bearer Token handling for Swagger

--- TFT-Friendly.Back/Bootstrap/Startup.cs
+++ TFT-Friendly.Back/Bootstrap/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
@@ -73,6 +74,33 @@ namespace TFT_Friendly.Back.Bootstrap
                     {
                         Name = "Tom COUSIN",
                         Email = "tom.cousin@epitech.eu"
+                    }
+                });
+                
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "@JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Scheme = "Bearer"
+                });
+                
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
                     }
                 });
                 
