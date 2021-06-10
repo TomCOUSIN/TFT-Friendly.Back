8700e49af7287c7311a964afba1295282a6bc759
tomcousin <tom.cousin@epitech.eu>
2021-06-10 19:54:41 +0900

    [~] Update UsersController by adding some exceptions handling and comments

--- TFT-Friendly.Back/Controllers/UsersController.cs
+++ TFT-Friendly.Back/Controllers/UsersController.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.ComTypes;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using TFT_Friendly.Back.Attributes;
+using TFT_Friendly.Back.Exceptions;
+using TFT_Friendly.Back.Models.Errors;
 using TFT_Friendly.Back.Models.Users;
-using TFT_Friendly.Back.Services.Mongo;
 using TFT_Friendly.Back.Services.Users;
 
 namespace TFT_Friendly.Back.Controllers
@@ -22,9 +19,8 @@ namespace TFT_Friendly.Back.Controllers
     public class UsersController : ControllerBase
     {
         #region MEMBERS
-        
+
         private readonly IUserService _userService;
-        private readonly ILogger<UsersController> _logger;
 
         #endregion MEMBERS
 
@@ -33,12 +29,10 @@ namespace TFT_Friendly.Back.Controllers
         /// <summary>
         /// Initialize a new <see cref="UsersController"/> class
         /// </summary>
-        /// <param name="logger">The logger to use</param>
         /// <param name="userService">The user service to manage users</param>
         /// <exception cref="ArgumentNullException">Throw an exception if one of the parameter is null</exception>
-        public UsersController(ILogger<UsersController> logger, IUserService userService)
+        public UsersController(IUserService userService)
         {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _userService = userService ?? throw new ArgumentNullException(nameof(userService));
         }
 
@@ -46,16 +40,43 @@ namespace TFT_Friendly.Back.Controllers
         
         #region ROUTES
 
+        /// <summary>
+        /// Get user information
+        /// </summary>
+        /// <returns>The information of the user</returns>
+        /// <response code="200">Everything worked well.</response>
+        /// <response code="401">Unauthorized request for this user.</response>
         [HttpGet("/me")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(HttpError), StatusCodes.Status401Unauthorized)]
         public IActionResult GetMe()
         {
             return Ok(_userService.GetMe((string)HttpContext.Items["UserId"]));
         }
 
+        /// <summary>
+        /// Patch the user information
+        /// </summary>
+        /// <param name="user">The new information of the user</param>
+        /// <returns>The user with new information</returns>
+        /// <response code="200">Everything worked well.</response>
+        /// <response code="400">Wrong information format.</response>
+        /// <response code="401">Unauthorized request for this user.</response>
         [HttpPatch("/me")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(HttpError), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(HttpError), StatusCodes.Status401Unauthorized)]
         public IActionResult PatchMe(User user)
         {
-            return Ok(_userService.PatchMe((string)HttpContext.Items["UserId"], user));
+            try
+            {
+                _userService.PatchMe((string) HttpContext.Items["UserId"], user);
+                return Ok(user);
+            }
+            catch (PasswordFormatException exception)
+            {
+                return BadRequest(new HttpError(StatusCodes.Status400BadRequest, exception.Message));
+            }
         }
 
         #endregion ROUTES
