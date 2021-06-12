2d41765c6929f66afa404c681790ad2a78d894cc
tomcousin <tom.cousin@epitech.eu>
2021-06-12 18:55:09 +0900

    [+] Add LeagueOfLegends User model

---
+++ TFT-Friendly.Back/Models/Users/LeagueOfLegendsUser.cs
@@ -0,0 +1,40 @@
+using Newtonsoft.Json;
+
+namespace TFT_Friendly.Back.Models.Users
+{
+    /// <summary>
+    /// LeagueOfLegendsUser class
+    /// </summary>
+    public class LeagueOfLegendsUser
+    {
+        /// <summary>
+        /// Id of League of legends user
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        /// <summary>
+        /// AccountId of League of legends user
+        /// </summary>
+        [JsonProperty("accountId")]
+        public string AccountId { get; set; }
+        
+        /// <summary>
+        /// Puuid of League of legends user
+        /// </summary>
+        [JsonProperty("puuid")]
+        public string Puuid { get; set; }
+        
+        /// <summary>
+        /// Name of League of legends user
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Level of League of legends user
+        /// </summary>
+        [JsonProperty("summonerLevel")]
+        public int SummonerLevel { get; set; }
+    }
+}
