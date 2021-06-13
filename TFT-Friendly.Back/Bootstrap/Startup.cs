d6d639ff513922cd2a96c5fa290c126579e24ad3
tomcousin <tom.cousin@epitech.eu>
2021-06-13 12:04:30 +0900

    [~] Update Startup by adding ItemService initialization

--- TFT-Friendly.Back/Bootstrap/Startup.cs
+++ TFT-Friendly.Back/Bootstrap/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.OpenApi.Models;
 using TFT_Friendly.Back.Clients;
 using TFT_Friendly.Back.Middlewares;
 using TFT_Friendly.Back.Models.Configurations;
+using TFT_Friendly.Back.Services.Items;
 using TFT_Friendly.Back.Services.Mongo;
 using TFT_Friendly.Back.Services.Users;
 
@@ -114,6 +115,7 @@ namespace TFT_Friendly.Back.Bootstrap
             });
             
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IItemService, ItemService>();
             services.AddSingleton<UsersContext>();
             services.AddSingleton<TftClient>();
         }
