3187656bd1b4c21b7caad55e6cf8fb1b6a3594fd
tomcousin <tom.cousin@epitech.eu>
2021-06-16 13:45:00 +0900

    [~] Update Item model by adding components for final items

--- TFT-Friendly.Back/Models/Items/Item.cs
+++ TFT-Friendly.Back/Models/Items/Item.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -44,5 +45,11 @@ namespace TFT_Friendly.Back.Models.Items
         /// </summary>
         [BsonElement("IsShadow")]
         public bool IsShadow { get; set; }
+        
+        /// <summary>
+        /// Components of the item
+        /// </summary>
+        [BsonElement("Components")]
+        public List<int> Components { get; set; }
     }
 }
