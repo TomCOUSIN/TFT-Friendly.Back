154d600a19906ad7465442ebb785ed5eb69a746b
tomcousin <tom.cousin@epitech.eu>
2021-07-27 11:36:58 +0200

    [+] Add Entity exception

--- TFT-Friendly.Back/Exceptions/EntityConflictException.cs
+++ TFT-Friendly.Back/Exceptions/EntityConflictException.cs
@@ -4,38 +4,37 @@ using System.Runtime.Serialization;
 namespace TFT_Friendly.Back.Exceptions
 {
     /// <summary>
-    /// ItemNotFoundException class
+    /// EntityConflictException class
     /// </summary>
     [Serializable]
-    public class ItemConflictException : Exception
+    public class EntityConflictException : Exception
     {
-
         #region CONSTRUCTOR
-
-        /// <summary>
-        /// Initialize a new <see cref="ItemConflictException"/> class
-        /// </summary>
-        public ItemConflictException() { }
         
         /// <summary>
-        /// Initialize a new <see cref="ItemConflictException"/> class
+        /// Initialize a new <see cref="EntityConflictException"/> class
+        /// </summary>
+        public EntityConflictException() { }
+        
+        /// <summary>
+        /// Initialize a new <see cref="EntityConflictException"/> class
         /// </summary>
         /// <param name="message">The message of the exception</param>
-        public ItemConflictException(string message) : base(message) { }
+        public EntityConflictException(string message) : base(message) { }
         
         /// <summary>
-        /// Initialize a new <see cref="ItemConflictException"/> class
+        /// Initialize a new <see cref="ChampionConflictException"/> class
         /// </summary>
         /// <param name="message">The message of the exception</param>
         /// <param name="innerException">The inner exception of the exception</param>
-        public ItemConflictException(string message, Exception innerException) : base(message, innerException) { }
+        public EntityConflictException(string message, Exception innerException) : base(message, innerException) { }
         
         /// <summary>
-        /// Initialize a new <see cref="ItemConflictException"/> class
+        /// Initialize a new <see cref="ChampionConflictException"/> class
         /// </summary>
         /// <param name="info">The serialization info of the exception</param>
         /// <param name="context">The streaming context of the exception</param>
-        protected ItemConflictException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        protected EntityConflictException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
         #endregion CONSTRUCTOR
     }
